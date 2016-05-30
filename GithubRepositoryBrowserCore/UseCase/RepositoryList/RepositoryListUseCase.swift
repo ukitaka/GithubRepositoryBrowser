5bc504e7c4e4322bcb4737b4fb85f92f69af30ca
ukitaka <yuki.takahashi.1126@gmail.com>
2016-05-30 16:48:26 +0900

    Implement RepositoryListUseCase

--- GithubRepositoryBrowserCore/UseCase/RepositoryList/RepositoryListUseCase.swift
+++ GithubRepositoryBrowserCore/UseCase/RepositoryList/RepositoryListUseCase.swift
@@ -6,26 +6,41 @@
 //  Copyright © 2016年 waft. All rights reserved.
 //
 
-import Foundation
 import RxSwift
 import RxCocoa
 
+public struct ReloadRepositoryListEvent {
+    let ownerName: String?
+}
+
 public protocol RepositoryListInputPort {
-    var reloadEvent: Observable<Void> { get }
+    var reloadEvent: Observable<ReloadRepositoryListEvent> { get }
 }
 
 public protocol RepositoryListOutputPort {
     var repositoryList: Observable<[Repository]> { get }
 }
 
-public struct RepositoryListUseCase: UseCase {
-    private let repos = Variable<[Repository]>([])
+public class RepositoryListUseCase: UseCase {
+    private let repos = BehaviorSubject<[Repository]>(value: [])
     private let disposeBag = DisposeBag()
     
     public init(inputPort: RepositoryListInputPort) {
-        //TODO: impl
+        inputPort.reloadEvent
+            .flatMap(findRepositories)
+            .bindTo(repos)
+            .addDisposableTo(disposeBag)
     }
     
+    private func findRepositories(e: ReloadRepositoryListEvent) -> Observable<[Repository]> {
+        if let name = e.ownerName {
+            return repositories.findByOwner(name)
+        } else {
+           return users.me().flatMap { [unowned self] u in
+                return self.repositories.findByOwner(u.login)
+            }
+        }
+    }
 }
 
 extension RepositoryListUseCase: RepositoryListOutputPort {
