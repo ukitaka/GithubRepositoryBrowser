f7926d89edb84d5ca09ce1fce121da0de78819bf
ukitaka <yuki.takahashi.1126@gmail.com>
2016-05-30 15:53:46 +0900

    [WIP] Add repository list usecase

--- GithubRepositoryBrowserCore/UseCase/RepositoryList/RepositoryListUseCase.swift
+++ GithubRepositoryBrowserCore/UseCase/RepositoryList/RepositoryListUseCase.swift
@@ -7,7 +7,29 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
-struct RepositoryListUseCase: UseCase {
+public protocol RepositoryListInputPort {
+    var reloadEvent: Observable<Void> { get }
+}
+
+public protocol RepositoryListOutputPort {
+    var repositoryList: Observable<[Repository]> { get }
+}
+
+public struct RepositoryListUseCase: UseCase {
+    private let repos = Variable<[Repository]>([])
+    private let disposeBag = DisposeBag()
     
+    public init(inputPort: RepositoryListInputPort) {
+        //TODO: impl
+    }
+    
+}
+
+extension RepositoryListUseCase: RepositoryListOutputPort {
+    public var repositoryList: Observable<[Repository]> {
+        return repos.asObservable()
+    }
 }
