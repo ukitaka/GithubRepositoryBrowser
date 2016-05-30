e244299dbcaeba9ae61ca80efa3205f6266ed39a
ukitaka <yuki.takahashi.1126@gmail.com>
2016-05-30 14:55:43 +0900

    Add usecases

---
+++ GithubRepositoryBrowserCore/UseCase/RepositoryList/RepositoryListUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoryListUseCase.swift
+//  GithubRepositoryBrowser
+//
+//  Created by Yuki Takahashi on 2016/05/30.
+//  Copyright © 2016年 waft. All rights reserved.
+//
+
+import Foundation
+
+struct RepositoryListUseCase: UseCase {
+    
+}
---
+++ GithubRepositoryBrowserCore/UseCase/UseCase.swift
@@ -0,0 +1,11 @@
+//
+//  UseCase.swift
+//  GithubRepositoryBrowser
+//
+//  Created by Yuki Takahashi on 2016/05/30.
+//  Copyright © 2016年 waft. All rights reserved.
+//
+
+import Foundation
+
+protocol UseCase {}
